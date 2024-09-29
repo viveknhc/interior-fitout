28780e55f32269fd6c3b9d1fdd829d08ce708636
vivek.z-at-596270005645 <vivek@zinbay.in>
2024-09-29 14:34:04 +0530

    29/09/24

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,112 @@
 
 var swiper = new Swiper(".service_slide", {
-    slidesPerView: 2,
-    spaceBetween: 30,
-    grabCursor: true,
-    loop: true,
-    navigation: {
-      nextEl: '.next-btn', // Custom next button
-      prevEl: '.prev-btn', // Custom prev button
+  slidesPerView: 2,
+  spaceBetween: 30,
+  grabCursor: true,
+  loop: true,
+  navigation: {
+    nextEl: '.next-btn', // Custom next button class
+    prevEl: '.prev-btn', // Custom prev button class
+  },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  breakpoints: {
+    200: {
+      slidesPerView: 1,
+      spaceBetween: 20
     },
-    breakpoints: {
-  
-      200: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-  
-  
-      // When window width is >= 640px
-      640: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-      // When window width is >= 768px
-      767: {
-        slidesPerView: 1,
-        spaceBetween: 30
-      },
-  
-      999: {
-        slidesPerView: 3,
-        spaceBetween: 30
-      },
-      // When window width is >= 1024px
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 40
-      }
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    767: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    999: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40
     }
-  });
+  }
+});
+
+
+
+
+var swiper = new Swiper(".clients_slide", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  grabCursor: true,
+  loop: true,
+  navigation: {
+    nextEl: '.next-btn', // Custom next button
+    prevEl: '.prev-btn', // Custom prev button
+  },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  breakpoints: {
+
+    200: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+
+
+    // When window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // When window width is >= 768px
+    767: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+
+    999: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    },
+    // When window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
+});
+
+
+var swiper = new Swiper(".testimonial_slide", {
+  // slidesPerView: 1.5,
+  centeredSlides: true,
+  speed: 1500,
+  // mousewheel: true,
+  grabCursor: true,
+
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    999: {
+      slidesPerView: 1.5,
+    }
+  }
+});
+
+
+// AOS
+
+AOS.init();
